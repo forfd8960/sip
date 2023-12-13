7c7ecc53b895c27a2f959490b53bd08f35f7fe79
forfd8960 <forfd8960@gmail.com>
2023-12-13 23:04:06 +0800

    feat: statement enum

--- sip/src/ast/mod.rs
+++ sip/src/ast/mod.rs
@@ -1,8 +1,33 @@
+use crate::tokens;
+use std::rc::Rc;
+
 pub trait Node {
     fn token_literal() -> String;
 }
 
-pub struct Program {}
+pub enum Expression {}
+
+pub enum Statement {
+    Identifier(tokens::Token),
+    VarStmt(tokens::Token, Expression), // var x = value
+    Assign(tokens::Token, Expression),  // x = value
+
+    // left, operator, right
+    Logical(Expression, tokens::Token, Expression),
+
+    // left, op, right: x + y
+    Binary(Expression, tokens::Token, Expression),
+
+    // op, value, -1, !true
+    Unary(tokens::Token, Expression),
+
+    // interger, string, true, false, etc
+    Literal(tokens::Token),
+}
+
+pub struct Program {
+    stmts: Vec<Statement>,
+}
 
 impl Node for Program {
     fn token_literal() -> String {
--- sip/src/parser/mod.rs
+++ sip/src/parser/mod.rs
@@ -3,7 +3,6 @@ use crate::{errors::ParserError, tokens::Token};
 #[derive(Debug)]
 pub struct Parser {
     tokens: Vec<Token>,
-    start: usize,
     current: usize,
 }
 
@@ -11,7 +10,6 @@ impl Parser {
     pub fn new(tokens: Vec<Token>) -> Self {
         Self {
             tokens: tokens,
-            start: 0 as usize,
             current: 0 as usize,
         }
     }
